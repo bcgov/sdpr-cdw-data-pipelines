f8aa875ab014bd4327c92617fde826d55c3079fe
joboso <jamesbscott@hotmail.ca>
2025-02-20 16:15:36 -0800

    reformatted em_user_id_to_emplid_xref_d compilation query

--- chips/chips_stg_to_cdw/em_user_id_to_emplid_xref_d/compile_src_data.sql
+++ chips/chips_stg_to_cdw/em_user_id_to_emplid_xref_d/compile_src_data.sql
@@ -1,11 +1,11 @@
-SELECT PS.OPRID,
-	PS.EMPLID,
-	PS.ROWSECCLASS,
-    PS.OPRDEFNDESC,
-    SS.OVERRIDE_DEPTID,
-    SS.DRILL_THROUGH_YN
-FROM CHIPS_STG.PS_OPRDEFN_BC_TBL PS
-LEFT OUTER JOIN STIIP_SECURITY_REQUIREMENT SS
-    on ps.emplid=ss.emplid
-WHERE PS.EMPLID IS NOT NULL
+select ps.oprid,
+	ps.emplid,
+	ps.rowsecclass,
+    ps.oprdefndesc,
+    ss.override_deptid,
+    ss.drill_through_yn
+from chips_stg.ps_oprdefn_bc_tbl ps
+left outer join chips_stg.stiip_security_requirement ss
+    on ps.emplid = ss.emplid
+where ps.emplid is not null
 ;
