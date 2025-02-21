7308a5d9604e10e685a4b50240266b369f2eee04
joboso <jamesbscott@hotmail.ca>
2025-02-21 14:07:05 -0800

    add filters on ps_pay_calendar to lookup query

--- chips/chips_stg_to_ods/org_employee_scds/lookup_current_load_pay_end_date.sql
+++ chips/chips_stg_to_ods/org_employee_scds/lookup_current_load_pay_end_date.sql
@@ -4,5 +4,8 @@ where pay_end_dt = (
   select max(pay_end_dt)
   from chips_stg.ps_pay_calendar
   where pay_begin_dt <= current_date
+    and pay_off_cycle_cal = 'N'
+    and paygroup = 'STD'
+    and trim(run_id) is not null
 )
 ;
