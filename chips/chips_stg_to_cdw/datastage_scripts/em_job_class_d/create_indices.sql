f99a2825ab0bbe5de7ea9ffc52a4fa37004be292
joboso <jamesbscott@hotmail.ca>
2025-03-12 09:29:30 -0700

    chips stg to cdw: added create index datastage script for em_job_class_d

---
+++ chips/chips_stg_to_cdw/datastage_scripts/em_job_class_d/create_indices.sql
@@ -0,0 +1,29 @@
+CREATE UNIQUE INDEX IJOB_CLASS_D_A1 ON EM_JOB_CLASS_D (JOBCLASS_SID)
+    tablespace CDW_INDX pctfree 10 initrans 2 maxtrans 255
+    storage (initial 10M  minextents 1  maxextents unlimited)
+    nologging compute statistics
+;
+
+CREATE INDEX IJOB_CLASS_D_A2 ON EM_JOB_CLASS_D (JOBCODE_BK)
+    tablespace CDW_INDX pctfree 10 initrans 2 maxtrans 255
+    storage (initial 10M minextents 1 maxextents unlimited)
+    nologging compute statistics
+;
+
+CREATE BITMAP INDEX IJOB_CLASS_D_A3 ON EM_JOB_CLASS_D (EMP_GROUP)
+    tablespace CDW_INDX pctfree 10 initrans 2 maxtrans 255
+    storage (initial 10M minextents 1 maxextents unlimited)
+    nologging compute statistics
+;
+
+CREATE BITMAP INDEX IJOB_CLASS_D_A4 ON EM_JOB_CLASS_D (INCL_EXCL)
+    tablespace CDW_INDX pctfree 10 initrans 2 maxtrans 255
+    storage (initial 10M  minextents 1 maxextents unlimited)
+    nologging compute statistics
+;
+
+CREATE BITMAP INDEX IJOB_CLASS_D_A5 ON EM_JOB_CLASS_D (JOB_FUNCTION)
+  tablespace CDW_INDX pctfree 10 initrans 2 maxtrans 255
+  storage (initial 10M  minextents 1 maxextents unlimited)
+  nologging compute statistics
+;
