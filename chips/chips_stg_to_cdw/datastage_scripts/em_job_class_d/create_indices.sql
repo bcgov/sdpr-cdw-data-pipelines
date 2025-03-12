27826d292396d2d8f14e8450563e9146aa154625
James Scott <james.b.scott@gov.bc.ca>
2025-03-12 10:36:58 -0700

    chips_stg to cdw: updated create indicies for em_jobb_class_d

--- chips/chips_stg_to_cdw/datastage_scripts/em_job_class_d/create_indices.sql
+++ chips/chips_stg_to_cdw/datastage_scripts/em_job_class_d/create_indices.sql
@@ -1,29 +1,34 @@
-CREATE UNIQUE INDEX IJOB_CLASS_D_A1 ON EM_JOB_CLASS_D (JOBCLASS_SID)
+CREATE UNIQUE INDEX cdw.IJOB_CLASS_D_A1 ON cdw.EM_JOB_CLASS_D (JOBCLASS_SID)
     tablespace CDW_INDX pctfree 10 initrans 2 maxtrans 255
     storage (initial 10M  minextents 1  maxextents unlimited)
     nologging compute statistics
 ;
+commit;
 
-CREATE INDEX IJOB_CLASS_D_A2 ON EM_JOB_CLASS_D (JOBCODE_BK)
+CREATE INDEX IJOB_CLASS_D_A2 ON cdw.EM_JOB_CLASS_D (JOBCODE_BK)
     tablespace CDW_INDX pctfree 10 initrans 2 maxtrans 255
     storage (initial 10M minextents 1 maxextents unlimited)
     nologging compute statistics
 ;
+commit;
 
-CREATE BITMAP INDEX IJOB_CLASS_D_A3 ON EM_JOB_CLASS_D (EMP_GROUP)
+CREATE BITMAP INDEX IJOB_CLASS_D_A3 ON cdw.EM_JOB_CLASS_D (EMP_GROUP)
     tablespace CDW_INDX pctfree 10 initrans 2 maxtrans 255
     storage (initial 10M minextents 1 maxextents unlimited)
     nologging compute statistics
 ;
+commit;
 
-CREATE BITMAP INDEX IJOB_CLASS_D_A4 ON EM_JOB_CLASS_D (INCL_EXCL)
+CREATE BITMAP INDEX IJOB_CLASS_D_A4 ON cdw.EM_JOB_CLASS_D (INCL_EXCL)
     tablespace CDW_INDX pctfree 10 initrans 2 maxtrans 255
     storage (initial 10M  minextents 1 maxextents unlimited)
     nologging compute statistics
 ;
+commit;
 
-CREATE BITMAP INDEX IJOB_CLASS_D_A5 ON EM_JOB_CLASS_D (JOB_FUNCTION)
+CREATE BITMAP INDEX IJOB_CLASS_D_A5 ON cdw.EM_JOB_CLASS_D (JOB_FUNCTION)
   tablespace CDW_INDX pctfree 10 initrans 2 maxtrans 255
   storage (initial 10M  minextents 1 maxextents unlimited)
   nologging compute statistics
 ;
+commit;
