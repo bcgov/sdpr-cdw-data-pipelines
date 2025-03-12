626c6ee953b1572d5087a21f939bc689875a0400
joboso <jamesbscott@hotmail.ca>
2025-03-12 10:45:08 -0700

    chips stg to cdw: removed index on jobclass_sid

--- chips/chips_stg_to_cdw/datastage_scripts/em_job_class_d/create_indices.sql
+++ chips/chips_stg_to_cdw/datastage_scripts/em_job_class_d/create_indices.sql
@@ -1,9 +1,9 @@
-CREATE UNIQUE INDEX cdw.IJOB_CLASS_D_A1 ON cdw.EM_JOB_CLASS_D (JOBCLASS_SID)
-    tablespace CDW_INDX pctfree 10 initrans 2 maxtrans 255
-    storage (initial 10M  minextents 1  maxextents unlimited)
-    nologging compute statistics
-;
-commit;
+-- CREATE UNIQUE INDEX cdw.IJOB_CLASS_D_A1 ON cdw.EM_JOB_CLASS_D (JOBCLASS_SID)
+--     tablespace CDW_INDX pctfree 10 initrans 2 maxtrans 255
+--     storage (initial 10M  minextents 1  maxextents unlimited)
+--     nologging compute statistics
+-- ;
+-- commit;
 
 CREATE INDEX IJOB_CLASS_D_A2 ON cdw.EM_JOB_CLASS_D (JOBCODE_BK)
     tablespace CDW_INDX pctfree 10 initrans 2 maxtrans 255
