07aec303c323502f2da5cc71ddc9f8a2342f0f87
James Scott <james.b.scott@gov.bc.ca>
2025-03-13 21:15:44 -0700

    chips stg to cdw: added batch script for build em_fte_burn_f

---
+++ chips/chips_stg_to_cdw/etl_jobs/build_fact_tables/build_em_fte_burn_f.bat
@@ -0,0 +1,5 @@
+@REM activate virtual environment
+call E:\ETL_V8\sdpr-cdw-data-pipelines\chips\chips-src-to-stg\.venv\Scripts\activate.bat
+
+@REM run python job script
+python "E:\ETL_V8\sdpr-cdw-data-pipelines\chips\chips_stg_to_cdw\etl_jobs\build_fact_tables\build_em_fte_burn_f.py"
