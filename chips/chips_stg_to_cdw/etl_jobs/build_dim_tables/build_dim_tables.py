ffeec20e3b0775aabd48593ddd897a4a7e038bd2
James Scott <james.b.scott@gov.bc.ca>
2025-03-14 16:57:04 -0700

    chips src to cdw: added new error handling options to run_sql_script

--- chips/chips_stg_to_cdw/etl_jobs/build_dim_tables/build_dim_tables.py
+++ chips/chips_stg_to_cdw/etl_jobs/build_dim_tables/build_dim_tables.py
@@ -28,7 +28,11 @@ tasks_dir = fr'{this_dir}\tasks\\'
 def run_sql_script(db: OracleDB, sql_script_endpoint: str):
     file_path = tasks_dir + sql_script_endpoint
     logger.info(f'executing: {sql_script_endpoint}')
-    db.run_sql_script(sql_file_path=file_path)
+    db.run_sql_script(
+        sql_file_path=file_path, 
+        ignore_all_db_errors=True,
+        # ora_codes_to_ignore=['ORA-02289'],
+    )
     logger.info(f'finished executing: {sql_script_endpoint}')
 
 def main():
