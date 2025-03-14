74f8e304982ca674bfae05c2607d6337f3eb20ff
James Scott <james.b.scott@gov.bc.ca>
2025-03-14 15:44:54 -0700

    chips src to stg: moved exit code position in etl script

--- chips/chips-src-to-stg/etl_jobs/chips_src_to_stg/chips_src_to_stg.py
+++ chips/chips-src-to-stg/etl_jobs/chips_src_to_stg/chips_src_to_stg.py
@@ -77,8 +77,8 @@ def main():
 if __name__ == "__main__":
     try:
         main()
-        sys.exit(0)
     except:
         logging.exception('Got exception on main handler')
         sys.exit(1)
         raise
+    sys.exit(0)
