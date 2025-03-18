0277906462e21c4362c8a0bf6bda7d8fd606a90e
joboso <jamesbscott@hotmail.ca>
2025-03-18 11:41:35 -0700

    chips stg to cdw: deleted datastage scripts as the python plus sql etl pipeline has replaced the datastage jobs

--- chips/chips_stg_to_cdw/datastage_scripts/em_paycode_d/compile_src_data.sql
+++
@@ -1,9 +0,0 @@
-select 
-    code_type,
-    class,
-    class_descr,
-    pay_cd,
-    pay_descr,
-    reduces_hours_worked
-from chips_stg.paycodes
-;
--- chips/chips_stg_to_cdw/datastage_scripts/stiip_security_requirement/compile_src_data.sql
+++
@@ -1,6 +0,0 @@
-select lpad(emplid, 6, '0') emplid,
-	name,
-	override_deptid,
-	drill_through_yn
-from chips_stg.stiip_security_requirement_stg
-;
