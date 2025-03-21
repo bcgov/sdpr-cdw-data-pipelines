e7d38929b4bc83776ce20ed664152c6f851c6e1d
joboso <jamesbscott@hotmail.ca>
2025-03-21 16:45:25 -0700

    chips stg to cdw: added test scripts

--- chips/chips_stg_to_cdw/etl_jobs/build_fact_tables/tests/test_em_fte_burn_f.sql
+++ chips/chips_stg_to_cdw/etl_jobs/build_fact_tables/tests/test_em_fte_burn_f.sql
@@ -43,3 +43,34 @@ select count(*)
 from cdw.em_fte_burn_f 
 where bu_sid is null
 ;
+
+select * 
+from cdw.em_employee_d
+where name like '&employee_name_to_lookup%'
+;
+
+select * 
+from cdw.em_fte_burn_f f
+left join cdw.em_appointment_status_d a on a.appt_status_sid = f.appointment_status_sid
+left join cdw.or_location_d using(location_sid)
+left join cdw.em_position_d using(position_sid)
+left join cdw.em_employee_d using(emplid)
+left join cdw.em_job_class_d j on j.jobclass_sid = f.job_class_sid
+left join cdw.or_business_unit_d using(bu_sid)
+where emplid = &enter_an_emplid
+order by pay_end_dt_sk desc
+;
+
+select * 
+from cdw.em_stiip_f f
+left join cdw.em_appointment_status_d a on a.appt_status_sid = f.appointment_status_sid
+left join cdw.or_location_d using(location_sid)
+left join cdw.em_employee_status_d using(empl_status_sid)
+left join cdw.em_position_d using(position_sid)
+left join cdw.or_business_unit_d using(bu_sid)
+left join cdw.em_employee_d using(emplid)
+left join cdw.em_job_class_d j using (jobclass_sid)
+left join cdw.em_paycode_d using(paycode_sid)
+where emplid = &enter_an_emplid
+order by pay_end_dt_sk desc
+;
