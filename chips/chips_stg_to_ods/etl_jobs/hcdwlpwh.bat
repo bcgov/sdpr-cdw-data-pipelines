6965cf81b7c8a3257275040e539301c399503445
James Scott <james.b.scott@gov.bc.ca>
2025-03-24 09:06:00 -0700

    chips stg to ods: updated hcdwlpwh

--- chips/chips_stg_to_ods/etl_jobs/hcdwlpwh.bat
+++ chips/chips_stg_to_ods/etl_jobs/hcdwlpwh.bat
@@ -1,52 +1,44 @@
 ECHO OFF
 setlocal & PUSHD & SET RET=
 rem  ---------------------------------------------------------------------------
-rem HCDWLPWH -- populates Employee Movement datat from MCHIPS_STG.
-rem          -- populated Organization Chart data from MCHIPS_STG.
-rem
 rem Description: 
-rem   This job controls the loading of Employee Movement data into the ODS.
-rem   This job controls the loading of Organization Chart data into the ODS.
-rem		
-rem		Data Transfering between CHIPS source and ODS performed via DM 
+rem   This job controls the loading of Employee Movement and Organization Chart 
+rem   data into the ODS from CHIPS_STG.
 rem  ---------------------------------------------------------------------------
 
 set JOB_NAME=%~n0
-set JOB_DESCRIPTION=Populate Employee Movement and Org Chart from CHIPS to ODS
+set JOB_DESCRIPTION=Populate Employee Movement and Org Chart from CHIPS_STG to ODS
 set OBJ_TYPE=ETL
 set APP_SYS=CHIPS
-set DS_PROJECT=HR
 
 CALL %ETL_BIN%\EnvironmentStart.bat
 IF %RET% NEQ 0 GOTO EXIT
+echo. >> %BATCH_LOG_FILE%
 
-:Job_Initiation 
-SET DS_PROCESS_NAME=Build_EMPLOYEE_ORGANIZATION
-rem ------------------------------------------------------------
-rem RUN Job Stream 
-rem ------------------------------------------------------------
-CALL %ETL_BIN%\DataStageJob.bat
+echo building employee movement >>%BATCH_LOG_FILE%  
+call E:\ETL_V8\sdpr-cdw-data-pipelines\chips\chips_stg_to_ods\etl_jobs\build_employee_movement\build_employee_movement.bat
 set RET=%ERRORLEVEL%
-ECHO %DS_PROCESS_NAME% return code is %RET% >>%BATCH_LOG_FILE%
-REM -----------------------------------------------------------------------------------------------------
-REM Check return code for data stage job run.  
-REM Only return code of 1 (Finished OK) and 2 (Finished with Warning) are acceptable.
-REM -----------------------------------------------------------------------------------------------------
-IF %RET% NEQ 1 IF %RET% NEQ 2 (
-    SET RET=16
-    GOTO EXIT
+echo python returned code %RET% >>%BATCH_LOG_FILE%
+rem Only return code of 0 is acceptable.
+for %%i in ("%~dp0..\..\") do set "chips_root_dir=%%~fi" 
+echo python log dir is set to: %chips_root_dir%chips_stg_to_ods\etl_jobs\build_employee_movement\build_employee_movement.log >>%BATCH_LOG_FILE% 
+if %RET% NEQ 0 (
+    echo failed to build employee movement in python >>%BATCH_LOG_FILE%
+    echo ----- start python log ----- >>%BATCH_LOG_FILE% 
+	type %chips_root_dir%chips_stg_to_ods\etl_jobs\build_employee_movement\build_employee_movement.log >>%BATCH_LOG_FILE% 
+	echo ------ end python log ------ >>%BATCH_LOG_FILE% 
+    set RET=16
+    GOTO FAILED
 )
-SET RET=0
-CALL %ETL_BIN%\data_handshake.bat COMPLETE "CHIPS Data"
+echo successfully built employee movement >>%BATCH_LOG_FILE%
+set RET=0
+echo. >> %BATCH_LOG_FILE%
 
+CALL %ETL_BIN%\data_handshake.bat COMPLETE "CHIPS Data"
 :EXIT
 set EXIT_CODE=%RET%
 
 CALL %ETL_BIN%\EnvironmentEnd.bat
-
 POPD & ENDLOCAL & SET EXIT_CODE=%EXIT_CODE%  & SET AGENT_EXE=%AGENT_EXE%
-
-rem ------------------------------------------------------------
 rem Send Return code back to ESP.
-rem ------------------------------------------------------------
-%AGENT_EXE% %EXIT_CODE%
+\%AGENT_EXE% %EXIT_CODE%
