0e304fa470308a9533e3c7ac9b5041629115f8c4
James Scott <james.b.scott@gov.bc.ca>
2025-03-26 17:34:23 -0700

    chips stg to ods: refactored build_employee_movement

--- chips/chips_stg_to_ods/etl_jobs/build_employee_movement/build_employee_movement.py
+++ chips/chips_stg_to_ods/etl_jobs/build_employee_movement/build_employee_movement.py
@@ -22,49 +22,53 @@ logging.basicConfig(
     style='{'
 )
 
+def run_sql_script(db: OracleDB, sql_script_endpoint: str):
+    tasks_dir = fr'{this_dir}\tasks\\'
+    file_path = tasks_dir + sql_script_endpoint
+    logger.info(f'executing: {sql_script_endpoint}')
+    db.run_sql_script(
+        sql_file_path=file_path, 
+        ignore_all_db_errors=True,
+        # ora_codes_to_ignore=['ORA-02289'],
+    )
+    logger.info(f'finished executing: {sql_script_endpoint}')
+
 def main():
     db = OracleDB(conn_str_key_endpoint = odb_conn_str_key_endpoint)
-    tasks_dir = r'E:\ETL_V8\sdpr-cdw-data-pipelines\chips\chips_stg_to_ods\etl_jobs\build_employee_movement\tasks\\'
-    sql_file_enpoints = [
-        r'em_temp_jcode.sql',
-        r'em_temp_fte_burn1.sql',
-        r'em_temp_fte_burn2.sql',
-        r'em_temp_act_reas1.sql',
-        r'em_temp_act_reas2.sql',
-        r'em_temp_dept_tab1.sql',
-        r'em_temp_actn_reas.sql',
-        r'em_temp_dept_tab2.sql',
-        r'em_temp_pos_dat1.sql',
-        r'em_temp_pos_dat2.sql',
-        r'em_temp_pos_num1.sql',
-        r'em_temp_last_pay.sql',
-        r'em_temp_q1.sql',
-        r'em_temp_q2.sql',
-        r'em_temp_q3.sql',
-        r'em_temp_q4.sql',
-        r'em_temp_empl_movement.sql',
-        r'em_temp_bu1.sql',
-        r'em_temp_bu2.sql',
-        r'em_temp_bu3.sql',
-        r'em_temp_bu4.sql',
-        r'em_temp_bu5.sql',
-        r'em_temp_bu6.sql',
-        r'em_temp_bu7.sql',
-        r'em_temp_bu8.sql',
-        r'em_temp_bu9.sql',
-        r'em_temp_bu10.sql',
-        r'em_temp_bu11.sql',
-        r'em_empl_movement_temp2.sql',
-        r'em_can_noc_tbl.sql',
-        r'em_employee_movement_t3.sql',
-        r'em_emplid_2_idir.sql',
-        r'em_empl_movement.sql',
-    ]
-    for endpoint in sql_file_enpoints:
-        file_path = tasks_dir + endpoint
-        logger.info(f'executing: {endpoint}')
-        db.run_sql_script(sql_file_path=file_path)
 
+    run_sql_script(db, r'em_temp_jcode.sql')
+    run_sql_script(db, r'em_temp_fte_burn1.sql')
+    run_sql_script(db, r'em_temp_fte_burn2.sql')
+    run_sql_script(db, r'em_temp_act_reas1.sql')
+    run_sql_script(db, r'em_temp_act_reas2.sql')
+    run_sql_script(db, r'em_temp_dept_tab1.sql')
+    run_sql_script(db, r'em_temp_actn_reas.sql')
+    run_sql_script(db, r'em_temp_dept_tab2.sql')
+    run_sql_script(db, r'em_temp_pos_dat1.sql')
+    run_sql_script(db, r'em_temp_pos_dat2.sql')
+    run_sql_script(db, r'em_temp_pos_num1.sql')
+    run_sql_script(db, r'em_temp_last_pay.sql')
+    run_sql_script(db, r'em_temp_q1.sql')
+    run_sql_script(db, r'em_temp_q2.sql')
+    run_sql_script(db, r'em_temp_q3.sql')
+    run_sql_script(db, r'em_temp_q4.sql')
+    run_sql_script(db, r'em_temp_empl_movement.sql')
+    run_sql_script(db, r'em_temp_bu1.sql')
+    run_sql_script(db, r'em_temp_bu2.sql')
+    run_sql_script(db, r'em_temp_bu3.sql')
+    run_sql_script(db, r'em_temp_bu4.sql')
+    run_sql_script(db, r'em_temp_bu5.sql')
+    run_sql_script(db, r'em_temp_bu6.sql')
+    run_sql_script(db, r'em_temp_bu7.sql')
+    run_sql_script(db, r'em_temp_bu8.sql')
+    run_sql_script(db, r'em_temp_bu9.sql')
+    run_sql_script(db, r'em_temp_bu10.sql')
+    run_sql_script(db, r'em_temp_bu11.sql')
+    run_sql_script(db, r'em_empl_movement_temp2.sql')
+    run_sql_script(db, r'em_can_noc_tbl.sql')
+    run_sql_script(db, r'em_employee_movement_t3.sql')
+    run_sql_script(db, r'em_emplid_2_idir.sql')
+    run_sql_script(db, r'em_empl_movement.sql')
 
 if __name__ == "__main__":
     try:
