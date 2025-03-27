7b14cbf6e5691955fcf5591c8670436e6a5a5133
joboso <jamesbscott@hotmail.ca>
2025-03-27 13:36:49 -0700

    chips stg to ods: added task to build employee_name

---
+++ chips/chips_stg_to_ods/etl_jobs/build_employee_tables/tasks/employee_name.sql
@@ -0,0 +1,30 @@
+drop table ods.employee_name purge;
+
+commit;
+
+create table ods.employee_name as
+    with 
+    names as (
+        select distinct emplid, name
+        from chips_stg.ps_pay_check 
+    ),
+    name_periods as (
+        select *
+        from names n
+        left join (
+            select p.emplid, p.name, min(p.pay_end_dt) first_pay_end_date, max(p.pay_end_dt) last_pay_end_date
+            from chips_stg.ps_pay_check p
+            group by emplid, name
+        ) using (emplid, name)
+    ),
+    rank_last_pay_end_date as (
+        select n.*, 
+            rank() over (partition by emplid order by last_pay_end_date desc) last_pay_end_date_rank
+        from name_periods n
+    )
+    select emplid, name, first_pay_end_date, last_pay_end_date,
+        case when last_pay_end_date_rank = 1 then 'Y' else 'N' end current_flg
+    from rank_last_pay_end_date a
+;
+
+commit;
